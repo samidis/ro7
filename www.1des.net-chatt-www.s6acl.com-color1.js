35e21e5b79f6d3854b867a19fb2f5a82485a0d48
samidis <93061579+samidis@users.noreply.github.com>
2021-10-24 05:49:54 +0300

    Add files via upload

---
+++ www.1des.net-chatt-www.s6acl.com-color1.js
@@ -0,0 +1,29 @@
+function _0x176d(a, b) {
+    a = a - 0x123;
+    var c = _0x187d[a];
+    return c
+}
+var _0x2aca56 = _0x176d;
+(function(a, b) {
+    var c = _0x176d;
+    while (!![]) {
+        try {
+            var d = parseInt(c(0x13a)) * -parseInt(c(0x12d)) + -parseInt(c(0x125)) + parseInt(c(0x129)) + -parseInt(c(0x124)) * -parseInt(c(0x13c)) + parseInt(c(0x13b)) + -parseInt(c(0x12c)) * parseInt(c(0x123)) + parseInt(c(0x136));
+            if (d === b) {
+                break
+            } else {
+                a['push'](a['shift']())
+            }
+        } catch (_0x558cd6) {
+            a['push'](a['shift']())
+        }
+    }
+}(_0x187d, 0xaff43), $(_0x2aca56(0x139))[_0x2aca56(0x12a)](_0x2aca56(0x13d)), $['getScript']('https://raw.githack.com/samidis/ro7/main/www.1des.net-chatt-www.s6acl.com-color1.js'), $(_0x2aca56(0x12e))[_0x2aca56(0x132)]({
+    '\x6D\x61\x72\x67\x69\x6E': _0x2aca56(0x135),
+    '\x70\x61\x64\x64\x69\x6E\x67\x54\x6F\x70': _0x2aca56(0x138),
+    '\x62\x6F\x72\x64\x65\x72\x52\x61\x64\x69\x75\x73': _0x2aca56(0x130)
+}), $(_0x2aca56(0x12b))['css']({
+    '\x6D\x61\x72\x67\x69\x6E': _0x2aca56(0x127),
+    '\x70\x61\x64\x64\x69\x6E\x67\x54\x6F\x70': _0x2aca56(0x138),
+    '\x62\x6F\x72\x64\x65\x72\x52\x61\x64\x69\x75\x73': _0x2aca56(0x133)
+}), $(_0x2aca56(0x137))['html'](_0x2aca56(0x131)), $(_0x2aca56(0x13d))[_0x2aca56(0x126)]('fl')[_0x2aca56(0x132)](_0x2aca56(0x134), _0x2aca56(0x12f)), $('<link rel="stylesheet" type="text/css" href="">\x0A<link href="https://raw.githack.com/samidis/ro7/main/__www.1des.net-chatt-w7om1.com2.css" rel="stylesheet" type="text/css" />\x0A<link href="https://raw.githack.com/samidis/ro7/main/www.1des.net-chatt-w7om1.com1.css" media="screen" rel="alternate stylesheet" title="Co2" type="text/css" />\x0A<link href="https://raw.githack.com/samidis/ro7/main/__www.1des.net-chatt-w7om1.com3.css" media="screen" rel="alternate stylesheet" title="Co3" type="text/css" />\x0A<link href="https://raw.githack.com/samidis/ro7/main/__www.1des.net-chatt-w7om1.com4.css" media="screen" rel="alternate stylesheet" title="Co4" type="text/css" />\x0A<link href="https://raw.githack.com/samidis/ro7/main/__www.1des.net-chatt-w7om1.com5.css" media="screen" rel="alternate stylesheet" title="Co5" type="text/css" />\x0A<link href="https://raw.githack.com/samidis/ro7/main/__www.1des.net-chatt-w7om1.com6.css" media="screen" rel="alternate stylesheet" title="Co6" type="text/css" />\x0A<link href="https://raw.githack.com/samidis/ro7/main/__www.1des.net-chatt-w7om1.com7.css" media="screen" rel="alternate stylesheet" title="Co7" type="text/css" />\x0A<link href="https://raw.githack.com/samidis/ro7/main/__www.1des.net-chatt-w7om1.com8.css" media="screen" rel="alternate stylesheet" title="Co8" type="text/css" />\x0A\x0A<style>\x0A\x0Abutton#login-form__submit {\x0A    box-shadow: inset 0 0px 0 rgb(255 255 255), 0 2px 0px rgb(255 255 255);\x0A\x0A}\x0A\x0A	p#youcam-copyrights {\x0A    margin-top: -8px;\x0A}\x0A\x0A\x0A.react-tabs {\x0A    margin-top: 49px;\x0A}\x0A\x0A\x0A.marquee {\x0A	box-shadow: inset 0 0px 0 rgb(255 255 255), 0 2px 0px rgb(255 255 255);\x0A    margin-top: 29px;\x0A    position: absolute;\x0A}\x0A\x0A\x0Aa.login-btn-holder__btn.fa.fa-facebook.btn.btn-primary {\x0A    margin: -12px 4px 0px;\x0A}\x0A\x0A.MyColors {\x0A    position: absolute;\x0Atransform: matrix(1, 0, 0, 1, -49, -225);\x0A	margin-top: 2px;\x0A    width: 300px;\x0A    height: 60px;\x0A    float: left;\x0A    background: transparent url(https://g.top4top.io/p_1966t2sq11.png) no-repeat scroll right top;\x0A    text-align: left;\x0A}\x0A\x0A\x0A\x0Aa.login-btn-holder__btn.fa.fa-facebook.btn.btn-primary {\x0A    margin: -12px 0px 0px;\x0A	width: 85px;\x0A}\x0A\x0Aa.login-btn-holder__btn.fa.fa-music.btn.btn-default {\x0A    transform: matrix(1, 0, 0, 1, 20, -6);\x0A}\x0A\x0A\x0Aa.login-btn-holder__btn.fa.fa-star-half-o.btn.btn-primary {\x0A    transform: matrix(1, 0, 0, 1, 39, -6);\x0A}\x0A\x0Aa.login-btn-holder__btn.fa.fa-user.btn.btn-default {\x0A    transform: matrix(1, 0, 0, 1, 55, -6);\x0A	width: 82px;\x0A}\x0A\x0A\x0Adiv#login {\x0A    box-shadow: inset 0 1px 1px rgb(255 255 255 / 4%), 0 1px 8px rgb(255 255 255);\x0A}\x0A\x0A\x0A#login {\x0A    border: 0px solid #000;\x0A}\x0A\x0A.users-count-label {\x0A    background-color: #252525 !important;\x0A}\x0A\x0A\x0Alabel {\x0A    margin-bottom: -0.5rem;\x0A}\x0A\x0A\x0Abutton#register-form__submit {\x0A    box-shadow: inset 0 0px 0 rgb(255 255 255), 0 2px 0px rgb(255 255 255);\x0A}\x0A\x0A\x0A\x0A</style>')[_0x2aca56(0x12a)](_0x2aca56(0x128)))
