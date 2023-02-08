0576fb43760cbd3eed4a5700b3ceda2994d23f87
samidis <93061579+samidis@users.noreply.github.com>
2023-02-08 18:41:19 +0300

    Update samids.js

--- samids.js
+++ samids.js
@@ -741,7 +741,7 @@ $("style").last().append(`
   background-size: cover;
   border: 1px solid #fff;
 }
-#users .uzr.stl1 .fl.muted.fa {
+#users .uzr.stl1 .fl.mini.u-msg {
   margin-left: -4px!important;
   float: left;
 }
@@ -774,9 +774,9 @@ var me_user1=setInterval(function(){
     if(myid!=null){
         clearInterval(me_user1);
         $("#users .uzr:contains('سامي')").addClass('stl1');
-        $("#users .uzr.stl1 .fl.muted.fa").append('<img class="My_star1" src="https://i.ibb.co/5G9Ttb7/10.gif">');
-	$("#users .uzr.stl1 .fl.muted.fa").append('<img class="My_tsor" src="https://i.ibb.co/ThKHx1N/image.png">');
-	$("#users .uzr.stl1 .fl.muted.fa").after('<div id="my_mood1" class="label label-primary" style=" margin-bottom: 1px; display: inline-block;width: 100%;margin-top: 10px;height: 22px;cursor: pointer;border-radius: 50px;padding: 0px;background-image: linear-gradient(-225deg, #000 0%, #000 29%, #660404 67%, #000 100%);" href="https://www.samidesign.net/">نتيجةُ صبرك إبتسامة، فلا تيأس.</div>');
+        $("#users .uzr.stl1 .fl.mini.u-msg").append('<img class="My_star1" src="https://i.ibb.co/5G9Ttb7/10.gif">');
+	$("#users .uzr.stl1 .fl.mini.u-msg").append('<img class="My_tsor" src="https://i.ibb.co/ThKHx1N/image.png">');
+	$("#users .uzr.stl1 .fl.mini.u-msg").after('<div id="my_mood1" class="label label-primary" style=" margin-bottom: 1px; display: inline-block;width: 100%;margin-top: 10px;height: 22px;cursor: pointer;border-radius: 50px;padding: 0px;background-image: linear-gradient(-225deg, #000 0%, #000 29%, #660404 67%, #000 100%);" href="https://www.samidesign.net/">نتيجةُ صبرك إبتسامة، فلا تيأس.</div>');
         $("#users .uzr span.u-topic:contains('سامي')").after('<img id="mystory1" class="My_vedio1" title="ستوري" src="https://i.ibb.co/47VS4nY/image.png">');
     
         $("#users .uzr.stl1 span.usid.fr, #users .uzr.stl1 img.fr").hide(); 
